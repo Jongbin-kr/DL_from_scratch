a7a2d0fd7dd1a1976e81ae8619b5453bbef81816
JONGBIN-SURFACE <wonjb001@naver.com>
2024-04-08 15:13:59 +0900

    edit `for` loop in numerical gradient  to `nditer`

--- Book1/common/gradient.py
+++ Book1/common/gradient.py
@@ -3,19 +3,20 @@ import numpy as np
 def numerical_gradient(f, x):
     grad = np.zeros_like(x)
     h = 1e-4
-    
-    for idx in range(x.size):
+    # print(x)
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
         tmp_val = x[idx]
-        
-        
         x[idx] = tmp_val + h
         fxh1 = f(x)
         
         x[idx] = tmp_val - h
         fxh2 = f(x)
-        
         grad[idx] = (fxh1 - fxh2) / (2*h)
+        
         x[idx] = tmp_val
+        it.iternext()
     
     return grad
 
